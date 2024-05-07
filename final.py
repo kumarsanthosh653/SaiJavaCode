9112869104580fe2500cad8989b17fbd5a1de83d
kumarsanthosh653 <109072065+kumarsanthosh653@users.noreply.github.com>
2024-05-07 18:22:22 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,7 +1,15 @@
 import requests
+import boto3
+import json
 
-# Define your API key
-api_key = '801231d4-b4ae-42e4-ba2c-53ebe9ae02c6'
+# Initialize Secrets Manager client
+client = boto3.client('secretsmanager')
+
+# Retrieve the secret value containing the API key
+secret_value = client.get_secret_value(SecretId="insightappsec/api-key")['SecretString']
+
+# Parse the JSON response to get the API key
+api_key = json.loads(secret_value).get('api_key')
 
 # Define the base URL for the API
 base_url = 'https://us3.api.insight.rapid7.com'
@@ -78,4 +86,87 @@ create_scan()
 # Check for vulnerabilities
 if check_vulnerabilities():
     print("Vulnerabilities found! Breaking the pipeline.")
-    raise Exception("Vulnerabilities found! Pipeline terminated.")
+    raise Exception("Vulnerabilities found! Pipeline terminated.") 
+
+
+# import requests
+
+# # Define your API key
+# api_key = '801231d4-b4ae-42e4-ba2c-53ebe9ae02c6'
+
+# # Define the base URL for the API
+# base_url = 'https://us3.api.insight.rapid7.com'
+
+# # Endpoint for authentication validation
+# validate_endpoint = '/validate'
+
+# # Endpoint to create a new scan
+# create_scan_endpoint = '/ias/v1/scans'
+
+# # Define headers for authentication
+# headers = {
+#     'X-Api-Key': api_key
+# }
+
+# # Function to perform authentication
+# def validate_api_key():
+#     # Construct the full URL for validation
+#     url = base_url + validate_endpoint
+
+#     # Make the GET request for validation
+#     response = requests.get(url, headers=headers)
+
+#     # Check if the request was successful
+#     if response.status_code == 200:
+#         print("Authentication successful!")
+#     else:
+#         print("Authentication failed. Status code:", response.status_code)
+
+# # Function to create a new scan
+# def create_scan():
+#     # Construct the full URL for creating a new scan
+#     url = base_url + create_scan_endpoint
+
+#     # Define the payload for the new scan
+#     payload = {
+#         "scan_config": {
+#             "id": "5fdaf09c-0eea-4324-8a7b-20ceb13365b9"
+#         },
+#         "scan_type": "REGULAR"
+#     }
+
+#     # Make the POST request to create a new scan
+#     response = requests.post(url, json=payload, headers=headers)
+
+#     # Check if the request was successful
+#     if response.status_code == 201:
+#         print("New scan created successfully!")
+#         try:
+#             json_response = response.json()
+#             if json_response:
+#                 print("Response:")
+#                 print(json_response)
+#             else:
+#                 print("No response content.")
+#         except ValueError:
+#             print("Response is not valid JSON.")
+#     else:
+#         print("Failed to create new scan. Status code:", response.status_code)
+
+
+# # Function to check vulnerabilities
+# def check_vulnerabilities():
+#     # Perform any logic to check for vulnerabilities
+#     # For demonstration purposes, let's assume vulnerabilities are more than 1
+#     return True
+    
+# # Perform authentication
+# validate_api_key()
+
+# # Create a new scan
+# create_scan()
+
+# # Check for vulnerabilities
+# if check_vulnerabilities():
+#     print("Vulnerabilities found! Breaking the pipeline.")
+#     raise Exception("Vulnerabilities found! Pipeline terminated.")
