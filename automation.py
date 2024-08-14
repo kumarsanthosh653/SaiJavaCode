9467cd4d203932e94037feb882f4927018aa9f77
kumarsanthosh653 <109072065+kumarsanthosh653@users.noreply.github.com>
2024-08-14 15:55:54 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,12 +1,13 @@
 import boto3
 import json
 import requests
+import time
 
 # Function to fetch the API key from AWS Systems Manager Parameter Store
 def get_api_key():
     ssm = boto3.client('ssm')
     try:
-        response = ssm.get_parameter(Name='/insightappsec/apikey', WithDecryption=True)
+        response = ssm.get_parameter(Name='/ohana-api/appspec-insights/api-key', WithDecryption=True)
         return response['Parameter']['Value']
     except Exception as e:
         print("Failed to retrieve parameter:", e)
@@ -14,7 +15,7 @@ def get_api_key():
 
 # Function to perform authentication
 def validate_api_key(api_key):
-    base_url = 'https://us3.api.insight.rapid7.com'
+    base_url = 'https://us2.api.insight.rapid7.com'
     validate_endpoint = '/validate'
     headers = {'X-Api-Key': api_key}
     url = base_url + validate_endpoint
@@ -29,11 +30,11 @@ def validate_api_key(api_key):
 
 # Function to create a new scan
 def create_scan(api_key):
-    base_url = 'https://us3.api.insight.rapid7.com'
+    base_url = 'https://us2.api.insight.rapid7.com'
     create_scan_endpoint = '/ias/v1/scans'
     headers = {'X-Api-Key': api_key, 'Content-Type': 'application/json'}
     payload = {
-        "scan_config": {"id": "2d01b271-151f-42dd-a65f-56d50de7cdd3"},
+        "scan_config": {"id": "3157bda2-1cc4-47fb-875d-973edd118f69"},
         "scan_type": "REGULAR"
     }
     url = base_url + create_scan_endpoint
@@ -46,8 +47,12 @@ def create_scan(api_key):
             if json_response:
                 print("Response:")
                 print(json_response)
-                # Check for vulnerabilities
-                check_vulnerabilities(api_key, json_response.get('scan_id'))
+                scan_id = json_response.get('scan_id')
+                if scan_id:
+                    # Wait for the scan to complete
+                    wait_for_scan_completion(api_key, scan_id)
+                else:
+                    print("Scan ID not found in response.")
             else:
                 print("No response content.")
         except ValueError:
@@ -55,9 +60,34 @@ def create_scan(api_key):
     else:
         print("Failed to create new scan. Status code:", response.status_code)
 
+# Function to wait for the scan to complete
+def wait_for_scan_completion(api_key, scan_id):
+    base_url = 'https://us2.api.insight.rapid7.com'
+    status_endpoint = f'/ias/v1/scans/{scan_id}'
+    headers = {'X-Api-Key': api_key}
+    url = base_url + status_endpoint
+
+    while True:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            scan_status = response.json().get('status')
+            if scan_status == 'Complete':
+                print("Scan completed successfully!")
+                check_vulnerabilities(api_key, scan_id)
+                break
+            elif scan_status in ['In Progress', 'Finalizing', 'Provisioning']:
+                print("Scan in progress... waiting...")
+                time.sleep(30)  # Wait before checking the status again
+            else:
+                print(f"Unexpected scan status: {scan_status}")
+                break
+        else:
+            print("Failed to get scan status. Status code:", response.status_code)
+            break
+
 # Function to check vulnerabilities and display messages based on severity
 def check_vulnerabilities(api_key, scan_id):
-    base_url = 'https://us3.api.insight.rapid7.com'
+    base_url = 'https://us2.api.insight.rapid7.com'
     vulnerabilities_endpoint = f'/ias/v1/scans/{scan_id}/vulnerabilities'
     headers = {'X-Api-Key': api_key}
     url = base_url + vulnerabilities_endpoint
